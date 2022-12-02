6ea722808169bffd312e8bf79536d9467ef692d8
Mojojojo0222 <102588760+Mojojojo0222@users.noreply.github.com>
2022-12-02 13:34:04 +0530

    first commit

--- models/Product.js
+++ models/Product.js
@@ -5,10 +5,8 @@ const ProductSchema = new mongoose.Schema(
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true, },
     img: { type: String, required: true },
-    //categories: { type: Array },
-    //size: { type: String },
-    //color: { type: String },
     price: { type: Number, required: true },
+    unit:{type:String, required:true }
     
   },
   //{ timestamps: true }
